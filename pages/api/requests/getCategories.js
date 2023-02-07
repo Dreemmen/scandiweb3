8cb7bfec5fca2cff5c9ffcdb3b3ccf37bf0b317a
ArnVig <s-word@inbox.lv>
2023-02-07 19:12:47 +0200

    4 commit

--- pages/api/requests/getCategories.js
+++ pages/api/requests/getCategories.js
@@ -1,5 +1,5 @@
 export default async function getCategories(host='http://localhost:3000'){
-    const apiUrlEndpoint = host + '/api/getCategories';
+    const apiUrlEndpoint = '/api/getCategories';
     const response = await fetch(apiUrlEndpoint);
     const json = await response.json();
     return json;
